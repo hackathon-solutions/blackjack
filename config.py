e3fc255394d424a3fb6c38ce3355a1041e1d93a3
jEUGENEdev <genyamateyuk@gmail.com>
2024-03-14 22:20:01 +0300

    added function for getting configuration

---
+++ config.py
@@ -0,0 +1,27 @@
+from json import loads
+
+MIN_BET = 'game.bet.min'
+MAX_BET = 'game.bet.max'
+INCREASE_ALLOW = 'game.bet.increase.allow'
+INCREASE_COUNT = 'game.bet.increase.count'
+PLACE_BACKGROUND = 'game.place.background'
+CARDS_SHEET = 'game.place.cards_style.sheet'
+HIDE_DEALER_CARDS = 'game.place.hide_dealer_cards'
+HIDE_FIRST_DEALER_CARD = 'game.place.hide_first_dealer_card'
+PLAYER_INIT_BALANCE = 'player.init.balance'
+DEALER_NAME = 'dealer.name'
+DEALER_DROP_FROM = 'dealer.behaviour.drop_from'
+
+
+with open('./conf.json', 'r', encoding='utf-8') as f:
+    _DATA = loads(f.read())
+
+
+def config(key: str):
+    """Allows you to get a value from the configuration by its key."""
+    global _DATA
+    keys = key.split('.')
+    data = _DATA[keys[0]]
+    for i in range(1, len(keys)):
+        data = data[keys[i]]
+    return data
