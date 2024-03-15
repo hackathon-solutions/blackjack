7fac4e98cbb1d9b1d9f60d05accc0d9a8d4828cd
jEUGENEdev <genyamateyuk@gmail.com>
2024-03-15 23:46:41 +0300

    launch frame (test)

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import sys
 
-from widgets.game import PlaySpace
-
 from PySide2 import QtWidgets
 
+from entities import Card, Game
+from widgets.game import PlaySpace, CardFrame
+from widgets.helpers import CardIdBuilder
+
+game = Game()
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    window = PlaySpace()
-    window.show()
+    main = PlaySpace()
+    layout = QtWidgets.QGridLayout(main)
+    card1 = CardFrame(CardIdBuilder(Card.SUIT_HEART, Card.RANK_10).get(), 100, 144)
+    card2 = CardFrame(CardIdBuilder(Card.SUIT_CLUB, Card.RANK_2).get(), 100, 144)
+    card3 = CardFrame(CardIdBuilder.cover().get(), 100, 144)
+    layout.addWidget(card1, 0, 0)
+    layout.addWidget(card2, 0, 1)
+    layout.addWidget(card3, 0, 2)
+    main.show()
+
     sys.exit(app.exec_())
