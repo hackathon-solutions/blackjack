e3e957bb03363d642c41d9d5cc347ddcb7372248
jEUGENEdev <genyamateyuk@gmail.com>
2024-03-15 20:40:36 +0300

    added background for game elements and created card image loader

--- widgets/game.py
+++ widgets/game.py
@@ -1,5 +1,31 @@
-from PySide2.QtWidgets import QWidget
+from PySide2 import QtWidgets, QtGui, QtCore
+
+from config import config, PLACE_BACKGROUND
+from widgets import helpers
 
 
-class PlaySpace(QWidget):
-    pass
+class PlaySpace(QtWidgets.QWidget):
+    def __init__(self, w: int = 900, h: int = 600):
+        super().__init__()
+        self._w = w
+        self._h = h
+        self.setWindowTitle('Блэкджек')
+        self.setMinimumSize(self._w, self._h)
+        self.setMaximumSize(self._w, self._h)
+
+    def paintEvent(self, arg__1):
+        pixmap = QtGui.QPixmap(config(PLACE_BACKGROUND)).scaled(self._w, self._h, QtCore.Qt.KeepAspectRatio)
+        QtGui.QPainter(self).drawPixmap(0, 0, pixmap)
+
+
+class Card(QtWidgets.QFrame):
+    def __init__(self, card_id: str, w: int = 64, h: int = 86):
+        super().__init__()
+        self._cover = helpers.CardImageLoader(card_id).get_image()
+        self._w = w
+        self._h = h
+
+    def paintEvent(self, arg__1):
+        painter = QtGui.QPainter(self)
+        pixmap = QtGui.QPixmap.fromImage(self._cover).scaled(self._w, self._h, QtCore.Qt.KeepAspectRatio)
+        painter.drawPixmap(0, 0, pixmap)
---
+++ widgets/helpers.py
@@ -0,0 +1,57 @@
+import os
+import pathlib
+
+from PySide2.QtGui import QImage
+
+import config
+
+
+class CardIdBuilder:
+    def __init__(self, suit: str, rank: str, mime: str | None = None):
+        self._suit = suit
+        self._rank = rank
+        self._mime = mime
+
+    def get(self):
+        return f'{self._rank}_{self._suit}{("." + self._mime) if self._mime else ""}'
+
+    SUIT_CLUB = 'club'
+    SUIT_DIAMOND = 'diamond'
+    SUIT_HEART = 'heart'
+    SUIT_SPADE = 'spade'
+    RANK_2 = '2'
+    RANK_3 = '3'
+    RANK_4 = '4'
+    RANK_5 = '5'
+    RANK_6 = '6'
+    RANK_7 = '7'
+    RANK_8 = '8'
+    RANK_9 = '9'
+    RANK_10 = '10'
+    RANK_JACK = 'J'
+    RANK_QUEEN = 'Q'
+    RANK_KING = 'K'
+    RANK_ACE = 'A'
+
+
+class CardImageLoader:
+    def __init__(self, card_id: str):
+        self._card_id = card_id
+
+    def get_path(self) -> pathlib.Path:
+        for file in os.listdir(config.config(config.CARDS_FOREGROUND)):
+            if file.startswith(self._card_id):
+                return pathlib.Path(f'{config.config(config.CARDS_FOREGROUND)}{file}').absolute()
+
+        raise FileNotFoundError(f'card with id "{self._card_id}" not found.')
+
+    def get_image(self) -> QImage:
+        return QImage(self.get_path().__str__())
+
+    @staticmethod
+    def get_cover_path() -> pathlib.Path:
+        return pathlib.Path(config.config(config.CARDS_BACKGROUND)).absolute()
+
+    @staticmethod
+    def get_cover_image() -> QImage:
+        return QImage(CardImageLoader.get_cover_path().__str__())
